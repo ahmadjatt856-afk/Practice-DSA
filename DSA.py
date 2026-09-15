008ce3d7cf74b959f006b572b018a465418bcb73
ahmadjatt856-afk <ahmadjatt856@gmail.com>
2026-09-15 08:25:58 -0700

    Create DSA.py
    this file is in python

---
+++ DSA.py
@@ -0,0 +1,72 @@
+# how many numbers smaller than the curent number
+
+# nums = [8,0,1,2,3]
+# list = []
+# for i in nums:
+#     c=0
+#     for j in nums:
+#         if j<i:
+#             c+=1
+#     list.append(c)  
+# print(list)      
+
+# count the digits that divide a number
+
+# n = 5183
+# while(n>0):
+#     r= n%10
+#     print(r,end=" ")
+#     n//=10
+
+# num = 5183
+# temp = num
+# ans = 0
+# while(temp>0):
+#         r = temp%10
+#         if num%r==0:
+#           ans+=1
+#         temp//=10
+# print(ans)
+
+# check a number is palandrome or not
+
+# x = 12321
+# temp = x
+# rev = 0
+
+# while(temp>0):
+#     r = temp%10
+#     temp//= 10
+#     rev = rev*10 + r
+# print(rev)
+# if rev==x:
+#     print("Panadrome")
+# else:
+#     print("not a palandrone")
+
+# subtract the product and sum of integer
+
+# n = 4421
+# temp = n
+# sum_= 0
+# product = 1
+# while(temp>0):
+#     r = temp%10
+#     temp//=10
+#     sum_+=r
+#     product*=r
+#     print(sum_)
+#     print(product)
+#     print(product-sum_)
+
+# kids with greatest number of candies
+ans = []
+i = 0
+candies = [2,3,5,2,1]
+extra = 3
+Maxcandies = max(i)
+
+for i in candies (i+extra) >= Maxcandies:
+ print(ans.appendtrue)
+else:
+ print("false")
